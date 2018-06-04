1b02f7fbeb2e20a344dd4ef6a7637de8b66959d1
sirvict0r <sirvict0r6@gmail.com>
2018-06-03 23:33:52 -0400

    fixing versions errors, basically third commit -___-

---
+++ jmad/solos/tests/test_url.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+from django.core.urlresolvers import resolve
+
+from jmad.solos.views import index
+
+
+class SolosUrlsTestCase(TestCase):
+    def test_root_url_uses_index_view(self):
+        """
+        Test that the root of the site resolvers to the
+        correct view function
+        :return:
+        """
+        root = resolve('/')
+        self.assertEqual(root.func, index)
